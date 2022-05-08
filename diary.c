939b78d1e9470283a5e855e017c629776d801ed2
muyaho-universe <kimdaesk@gmail.com>
2022-05-08 17:49:11 +0900

    Update DeletePost function and Save Board function

--- diary.c
+++ diary.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdbool.h>
 #include "diary.h"
 
 int CreatePost(Post *p, int index)
@@ -10,7 +11,7 @@ int CreatePost(Post *p, int index)
     char year[5] = "";
     char mon[3] = "";
     char day[3] = "";
-
+    
     time_t t = time(NULL);
     struct tm *tm = localtime(&t);
 
@@ -25,9 +26,16 @@ int CreatePost(Post *p, int index)
     printf("내용\n");
     scanf("%[^\n]s", p->content);
     getchar();
-
-    printf("첨부 사진 여부 (Y/N)\n");
-    scanf("%c", &p->isAttached);
+    do
+    {
+        printf("첨부 사진 여부 (Y/N)\n");
+        scanf("%c", &p->isAttached);
+        if (p->isAttached == 'Y' || p->isAttached == 'N')
+            break;
+        printf("\'Y\'나 \'N\'으로 입력해주세요\n");
+    } while (true);
+    
+    
 
     sprintf(year, "%d", tm->tm_year + 1900);
     sprintf(mon, "%d", tm->tm_mon + 1);
@@ -43,6 +51,7 @@ int CreatePost(Post *p, int index)
 
     p->num = index;
 
+    p->isDeleted = false;
     return 1;
 }
 
@@ -66,13 +75,26 @@ int UpdatePost(Post *p, int count, int idx)
 {
     return 0;
 }
-int DeletePost(Post *p, int idx)
+int DeletePost(Post *p, int idx)    
 {
-    return 0;
+    p[idx].isDeleted = true;
+    return 1;
 }
-void SaveBoard(Post *p)
+void SaveBoard(Post *p, int idx)
 {
-    return;
+    char filename[20] = "dairy.txt";
+    FILE * fp;
+    fp = fopen(filename, "w");
+    int j = 1;
+    for (int i = 0; i < idx; i++)
+    {
+        if (!p[i].isDeleted)
+        {
+            fprintf(fp,"%d\t%s\t%s\t%s\n", j, p[i].title, p[i].date, p[i].name);
+            fprintf(fp,"%s\n", p[i].content);
+            j++;
+        }
+    }
 }
 int LoadBoard(Post *p)
 {
@@ -81,13 +103,13 @@ int LoadBoard(Post *p)
 int ShowMenu()
 {
     int menu;
-    printf("\n*** 점수계산기 ***\n");
-    printf("1. 조회\n");
-    printf("2. 상세보기\n");
-    printf("3. 추가\n");
-    printf("4. 수정\n");
-    printf("5. 삭제\n");
-    printf("6. 저장\n");
+    printf("\n*** My Mood Diary ***\n");
+    printf("1. 일기조회\n");
+    printf("2. 내용 상세보기\n");
+    printf("3. 글 추가\n");
+    printf("4. 글 수정\n");
+    printf("5. 글 삭제\n");
+    printf("6. 현재 내용 저장\n");
     printf("7. 찾기\n");
     printf("0. 종료\n\n");
     printf("=> 원하는 메뉴는? ");
@@ -108,8 +130,15 @@ int SelectPostNo(Post *p, int index)
 {
     int idx;
     ListBoard(p, index);
-    printf("번호는 (취소 : 0)? ");
-    scanf("%d", &idx);
-    getchar();
+    while (true)
+    {
+        printf("번호는 (취소 : 0)? ");
+        scanf("%d", &idx);
+        getchar();
+        if (idx <= index)
+            break;
+        printf("해당 데이터는 없습니다.\n");
+    }
+    
     return idx;
 }
--- diary.h
+++ diary.h
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 typedef struct
 {
     int num;
@@ -6,6 +8,7 @@ typedef struct
     char date[11];
     char content[100];
     char isAttached; // Y or N
+    bool isDeleted;
 } Post;
 
 int CreatePost(Post *p, int index);          // create a post
@@ -13,7 +16,7 @@ void ReadPost(Post p);                       // read a post
 void ListBoard(Post *p, int idx);            // list all posts in board
 int UpdatePost(Post *p, int count, int idx); // update a post
 int DeletePost(Post *p, int idx);            // delete a post
-void SaveBoard(Post *p);                     // save all posts in board
+void SaveBoard(Post *p, int idx);                     // save all posts in board
 int LoadBoard(Post *p);                      // load all posts in board
 int ShowMenu();                              // show menus
 void SearchPost(Post *p, int idx);           // searching in board
